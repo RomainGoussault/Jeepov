2f7149937354e1df94b748339dc722baa29a878e
Romain Goussault <romain.goussault@gmail.com>
2014-07-03 20:33:46 +1000

    Add King, Queen, Knight and Pawn classes

---
+++ Deepov/src/com/gousslegend/deepov/pieces/Knight.java
@@ -0,0 +1,24 @@
+package com.gousslegend.deepov.pieces;
+
+import java.util.List;
+
+import com.gousslegend.deepov.Board;
+import com.gousslegend.deepov.Move;
+
+public class Knight extends Piece
+{
+
+    public Knight(Position position, Board board, boolean isBlack)
+    {
+	super(position, board, isBlack);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public List<Move> getLegalMoves()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
